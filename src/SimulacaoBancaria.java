c57a4defffd2f3ffb3d58bcabbf188c8dc0640a1
dlucioyauh <dlucio.douglas@gmail.com>
2024-06-15 14:53:53 -0300

    Simulando conta com switch

---
+++ src/SimulacaoBancaria.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.printf("Saldo atual: %.1f%n", saldo);
+                    break;
+                case 2:
+
+                    double valorSaque = scanner.nextDouble();
+                    if (valorSaque > saldo) {
+                        System.out.println("Saldo insuficiente.");
+                    } else {
+                        saldo -= valorSaque;
+                        System.out.printf("Saldo atual: %.1f%n", saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.printf("Saldo atual: %.1f%n", saldo);
+                    break;
+                case 0:
+                    continuar = false;
+                    System.out.println("Programa encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
